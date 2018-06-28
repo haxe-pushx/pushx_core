52e058ec89664c4582c6c5bae376583d8b53a52b
Kevin Leung <kevinresol@gmail.com>
2018-06-28 13:20:40 +0800

    Distinguish errors

--- src/pushx/Result.hx
+++ src/pushx/Result.hx
@@ -3,5 +3,10 @@ package pushx;
 using tink.CoreApi;
 
 typedef Result = {
-	errors: Array<{id:String, error:Error}>
+	errors: Array<{id:String, type:ErrorType}>
+}
+
+enum ErrorType {
+	InvalidTarget;
+	Others(err:Error);
 }
